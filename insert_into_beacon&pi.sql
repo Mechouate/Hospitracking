fc40f54dd1a05c2f2014568a3bdc2805142eb9d3
Adam Mechouate <a.mechouate@hospicall.com>
2019-01-18 15:04:54 +0100

    insert into beacons update

--- insert_into_beacon&pi.sql
+++ insert_into_beacon&pi.sql
@@ -14,6 +14,13 @@ insert into beacon(beacon_mac) VALUES ("AC:23:3F:24:AF:81");
 insert into beacon(beacon_mac) VALUES ("AC:23:3F:24:AF:77");
 insert into beacon(beacon_mac) VALUES ("AC:23:3F:24:A1:D2");
 insert into beacon(beacon_mac) VALUES ("AC:23:3F:24:A2:0E");
+insert into beacon(beacon_mac) VALUES ("AC:23:3F:24:A2:0E");
+insert into beacon(beacon_mac) VALUES ("C0:AD:72:70:5B:AB");
+insert into beacon(beacon_mac) VALUES ("CC:16:08:85:7F:99");
+insert into beacon(beacon_mac) VALUES ("C7:E9:5F:BE:F5:82");
+insert into beacon(beacon_mac) VALUES ("C8:2E:E5:77:F2:AB");
+insert into beacon(beacon_mac) VALUES ("E6:6A:0E:B6:A8:21");
+insert into beacon(beacon_mac) VALUES ("FC:BB:10:74:CF:70");
 
 MariaDB [raum]> select * from beacon;
 +----+-------------------+
@@ -34,8 +41,15 @@ MariaDB [raum]> select * from beacon;
 | 13 | AC:23:3F:24:AF:84 |
 | 14 | AC:23:3F:24:AF:81 |
 | 15 | AC:23:3F:24:AF:77 |
+| 16 | AC:23:3F:24:A1:D2 |
+| 17 | AC:23:3F:24:A2:0E |
+| 18 | C0:AD:72:70:5B:AB |
+| 19 | CC:16:08:85:7F:99 |
+| 20 | C7:E9:5F:BE:F5:82 |
+| 21 | C8:2E:E5:77:F2:AB |
+| 22 | E6:6A:0E:B6:A8:21 |
 +----+-------------------+
-19 rows in set (0.00 sec)
+22 rows in set (0.00 sec)
 
 --------------------------------------------------------------------------
 
@@ -85,7 +99,12 @@ Vorhandene Beacon	Beschreibung
 AC:23:3F:24:B3:1F	Neu Muenze
 AC:23:3F:24:B3:18	Neu Muenze
 AC:23:3F:24:B2:EC	Neu Muenze
-C8:2E:E5:77:F2:AB 	Alt Beacon Muenze
+C0:AD:72:70:5B:AB   Alte Beacon Muenze A1
+CC:16:08:85:7F:99   Alte Beacon Muenze B2
+C7:E9:5F:BE:F5:82   Alte Beacon Muenze C3
+C8:2E:E5:77:F2:AB 	Alte Beacon Muenze
+E6:6A:0E:B6:A8:21   Alte Beacon Muenze 
+FC:BB:10:74:CF:70   Alte Beacon Muenze 
 AC:23:3F:24:98:CB	on/off drinnen
 AC:23:3F:24:99:46	on/off drinnen
 AC:23:3F:24:99:57	on/off drinnen
@@ -93,9 +112,9 @@ AC:23:3F:24:CC:E9 	on/off draussen
 AC:23:3F:24:CC:EF	on/off draussen
 AC:23:3F:24:CC:F1	on/off draussen
 AC:23:3F:24:C8:AD 	Gross Schwarz Beacon
-AC:23:3F:24:A1:D4	Gross Weiß Beacon
-AC:23:3F:24:A1:D2   Gross Weiß Beacon
-AC:23:3F:24:A2:0E   Gross Weiß Beacon
+AC:23:3F:24:A1:D4	Gross Weiss Beacon
+AC:23:3F:24:A1:D2   Gross Weiss Beacon
+AC:23:3F:24:A2:0E   Gross Weiss Beacon
 AC:23:3F:24:AF:84	Die Ganz duenne Beacon E8 out
 AC:23:3F:24:AF:81 	Die Ganz duenne Beacon E8 alt
 AC:23:3F:24:AF:77 	Die Ganz duenne Beacon E8 neu
